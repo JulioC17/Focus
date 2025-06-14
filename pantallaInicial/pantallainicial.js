71731479f85f33780b122b6f5c8097793c3402bd
Julio <juliocsreyes94@gmail.com>
2025-06-14 17:47:42 +0200

    pequenio stop

---
+++ pantallaInicial/pantallainicial.js
@@ -0,0 +1,10 @@
+const bigContainer = document.createElement('div')
+bigContainer.classList.add('container-fluid', 'bg-light')
+
+const mainContainer = document.createElement('div')
+mainContainer.classList.add('text-dark')
+
+const mainTittle = document.createElement('h1')
+mainTittle.textContent = FOCUS
+mainTittle.classList.add('text-dark', 'fs2')
+
